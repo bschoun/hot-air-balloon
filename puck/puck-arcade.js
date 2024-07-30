19dff74e184ab2223161a937a29542fdf0d8bfc9
bre.schoun <breawn.schoun@childrenscolorado.org>
2024-07-30 14:32:16 -0600

    Added forward-tilt puck control of hot air balloon

---
+++ puck/puck-arcade.js
@@ -0,0 +1,28 @@
+// Add the "Connect Puck.js" link/button to the footer
+var footer = document.getElementById("footer");
+footer.innerHTML = "<button id=\"btnConnect\">Connect Puck.js</button>" + footer.innerHTML;
+
+var puckJsScript = document.createElement('script');
+puckJsScript.type = 'text/javascript';
+puckJsScript.src = 'https://www.puck-js.com/puck.js';
+document.body.appendChild(puckJsScript);
+
+var ahrsScript = document.createElement('script');
+ahrsScript.type = 'text/javascript';
+ahrsScript.src = 'puck/www-ahrs.js';
+document.body.appendChild(ahrsScript);
+
+var arcadeInputScript = document.createElement('script');
+arcadeInputScript.type = 'text/javascript';
+arcadeInputScript.src = 'puck/arcade-input.js';
+document.body.appendChild(arcadeInputScript);
+
+var puckInputScript = document.createElement('script');
+puckInputScript.type = 'text/javascript';
+puckInputScript.src = 'puck/puck-input.js';
+document.body.appendChild(puckInputScript);
+
+var puckCss = document.createElement('link');
+puckCss.rel = 'stylesheet';
+puckCss.href = 'puck/puck-arcade.css';
+document.body.appendChild(puckCss);
